ba296ec2cebe96760cf65c4a404bdd1e59116787
User <nikita96max@gmail.com>
2022-09-23 22:51:40 +0500

    assign to callback buttons values from CallbackButtonEnum

--- src/main/java/ru/nexonmi/DiaryBotNexonmi/botapi/updates/callback/CallbackEnum.java
+++ src/main/java/ru/nexonmi/DiaryBotNexonmi/botapi/updates/callback/CallbackEnum.java
@@ -1,14 +1,15 @@
 package ru.nexonmi.DiaryBotNexonmi.botapi.updates.callback;
 
-public enum CallbackEnum {
+enum CallbackEnum {
 
     //TODO make all callback buttons use this variables not straight str
-    ADD_LESSON_TO_TIMETABLE_CHOSE_DAY("command.callback.add_lesson_to_timetable_choose_day", "oops"),
+    ADD_LESSON_TO_TIMETABLE_CHOOSE_DAY("command.callback.add_lesson_to_timetable_choose_day", "replay.callback.add_lesson_to_timetable_chose_day"),
     ADD_LESSON_TO_DAY_CHOOSE_LESSON("command.callback.add_lesson_to_day_choose_lesson", "oops"),
-    ADD_LESSON_TO_DAY("command.callback.add_lesson_to_day_add_lesson", "oops"),
+    ADD_LESSON_TO_DAY_ADD_LESSON("command.callback.add_lesson_to_day_add_lesson", "oops"),
     SHOW_ALL_TIMETABLE("command.callback.show_all_timetable", "replay.callback.show_all_message_title"),
     TIMETABLE_EDIT("command.callback.timetable_edit", "replay.callback.timetable_edit"),
-    DELETE_LESSON_CHOOSE_DAY("command.callback.delete_lesson_choose_day", "replay.callback.delete_lesson_chose_day");
+    DELETE_LESSON_CHOOSE_DAY("command.callback.delete_lesson_choose_day", "replay.callback.delete_lesson_chose_day"),
+    DELETE_LESSON_CHOOSE_LESSON("nul", "command.callback.delete_lesson_choose_lesson");
 
     String commandCode;
     String replayCode;
